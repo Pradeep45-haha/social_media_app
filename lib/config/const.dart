88a854e7fcafcf46be594d08cae220c3c194e808
Pradeep Sharma <75775013+Pradeep45-haha@users.noreply.github.com>
2024-08-24 16:49:38 +0530

    Update const.dart

--- lib/config/const.dart
+++ lib/config/const.dart
@@ -1,14 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-FirebaseOptions firebaseOptions = const FirebaseOptions(
-  apiKey: "AIzaSyDLG8OC43BX2Iq3DN5KP7Lh0URXHRREdv8",
-  authDomain: "instagram-5d717.firebaseapp.com",
-  projectId: "instagram-5d717",
-  storageBucket: "instagram-5d717.appspot.com",
-  messagingSenderId: "78708088802",
-  appId: "1:78708088802:web:f94e1cc70164c8289523bb",
-);
+
 
 TextStyle textStyle0 =
     const TextStyle(fontSize: 20, fontWeight: FontWeight.bold);
